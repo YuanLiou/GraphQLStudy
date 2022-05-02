8f1ba55be304f886890c16491d04f5e3e8b63e94
Ray Yuan Liu <louis383@gmail.com>
2022-05-02 17:39:59 +0800

    Add movie schema and query ready for quering.

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,8 +1,9 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
 const _ = require("lodash");
 
 const resolvers = {
     Query: {
+        // USER RESOLVERS
         // you can define like this `users() {}`, too
         users: () => {
             return UserList;
@@ -11,7 +12,17 @@ const resolvers = {
             const id = args.id;
             const user = _.find(UserList, { id: Number(id) });
             return user;
-        }
+        },
+
+        // MOVIE RESOLVERS
+        movies: () => {
+            return MovieList;
+        },
+        movie: (parent, args) => {
+            const name = args.name;
+            const movie = _.find(MovieList, { name })
+            return movie;
+        },
     },
 };
 
